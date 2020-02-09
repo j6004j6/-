8e0e5f08e81951e70f3c3191ccb0e13ee3f5d95f
Justin Wu <j6309j6@hotmail.com>
2020-02-09 15:16:35 +0800

    first

---
+++ viewmodel/viewmodel/Models/CUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iii.CUser
+{
+    public class CUser
+    {
+        public string fId { get; set; }
+        public string fAccount { get; set; }
+        public string fPassword { get; set; }
+        public string fName { get; set; }
+        public string fDepartment { get; set; }
+        public string fAuthorization { get; set; }
+        public string fStatus { get; set; }
+        public string fArrivingDate { get; set; }
+        public string fLeavingDate { get; set; }
+        public string fEmail { get; set; }
+        public string fPhone { get; set; }
+        public string fAddress { get; set; }
+    }
+}
---
+++ viewmodel/viewmodel/ViewModel/CLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace viewmodel.ViewModel
+{
+    public class CLogin
+    {
+        public string txtId { get; set; }
+        public string txtAccount { get; set; }
+        public string txtPassword { get; set; }
+      
+    }
+}
