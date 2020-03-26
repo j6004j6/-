ae4530a7f363c4467f5b8a16c51573810c5baf5f
Justin Wu <j6309j6@hotmail.com>
2020-03-26 16:03:11 +0800

    20200326
    傳送專題

---
+++ 20200312/TestFirst/Models/CDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestFirst.Models
+{
+    public class CDate
+    {
+        static void Main()
+        {
+            DateTime today = DateTime.Now;
+            DateTime answer = today.AddDays(36);
+            Console.WriteLine("Today: {0:dddd}", today);
+            Console.WriteLine("36 days from today: {0:dddd}", answer);
+        }
+    }       
+ }
---
+++ 20200312/TestFirst/Models/chartMission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestFirst.Models
+{
+    public class chartMission
+    {
+        public int fId { get; set; }
+        public int fStage { get; set; }
+        public int fEst_Time { get; set; }
+    }
+}
